206759f54ae49632939daac6e4e4de0d99e49487
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-07-25 15:51:52 -0400

    Created the Deck class

---
+++ COMP123-S2017-Lesson11A/Deck.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Tom Tsiliopoulos
+ * Date: July 25, 2017
+ * Description: This is the Deck class which inherits from the List generic collection
+ * This class creates a new List type - Card
+ * Version: 0.1 - Created the Deck class
+ */
+
+namespace COMP123_S2017_Lesson11A
+{
+    public class Deck:List<Card>
+    {
+        // PRIVATE INSTANCE VARIABLES
+
+        // PUBLIC PROPERTIES
+
+        // CONSTRUCTORS
+
+        /// <summary>
+        /// This is the main constructor for the Deck class.
+        /// </summary>
+        public Deck()
+        {
+            this._initialize();
+        }
+
+        // PRIVATE METHODS
+
+        /// <summary>
+        /// This is the private _initialize method that loads the deck with 52 cards.
+        /// </summary>
+        private void _initialize()
+        {
+            for (int suit = 0; suit < (int)Suit.Spades; suit++)
+            {
+                for (int face = 1; face < (int)Face.King; face++)
+                {
+                    this.Add(new Card((Face)face, (Suit)suit));
+                }
+            } 
+        }
+
+        // PUBLIC METHODS
+
+        /// <summary>
+        /// This method overrides the built-in ToString method and outputs the current contents
+        /// of the deck.
+        /// </summary>
+        public override string ToString()
+        {
+            string outputString = "";
+
+            foreach (Card card in this)
+            {
+                outputString += "The " + card.Face + " of " + card.Suit + "\n";
+            }
+
+            return outputString;
+        }
+    }
+}
