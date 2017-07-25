1aa8207e4ba0f1cf9c2d7dc646fc0ccb38345f42
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-07-25 15:54:00 -0400

    Updated the Program class to implement / test the Deck class

--- COMP123-S2017-Lesson11A/Program.cs
+++ COMP123-S2017-Lesson11A/Program.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
  * Name: Tom Tsiliopoulos
  * Date: July 25, 2017
  * Description: Demo for Lesson 11
- * Version: 0.1 - Created the Program class
+ * Version: 0.2 - Implemented the Deck class
  */
 
 namespace COMP123_S2017_Lesson11A
@@ -17,6 +17,8 @@ namespace COMP123_S2017_Lesson11A
     {
         static void Main(string[] args)
         {
+            Deck deck = new Deck();
+            Console.WriteLine(deck.ToString());
         }
     }
 }
