e5e12507e83245b61badea32b542de0b43681f50
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-07-25 15:26:46 -0400

    Created the Card class

---
+++ COMP123-S2017-Lesson11A/Card.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/*
+ * Name: Tom Tsiliopoulos
+ * Date: July 25, 2017
+ * Description: This is the Card class to be used in the
+ * card shuffling and dealing simulation
+ * Version: 0.1 - Created the Card class
+ */
+
+namespace COMP123_S2017_Lesson11A
+{
+    public class Card
+    {
+        // PRIVATE INSTANCE VARIABLES
+        private Face _face;
+        private Suit _suit;
+
+        // PUBLIC PROPERTIES
+        public Face Face {
+
+            get
+            {
+                return this._face;
+            }
+
+
+            set
+            {
+                this._face = value;
+            }
+                
+                
+         }
+
+        public Suit Suit {
+
+            get
+            {
+                return this._suit;
+            }
+
+            set
+            {
+                this._suit = value;
+            }
+
+
+        }
+
+        // CONSTRUCTORS
+
+        /// <summary>
+        /// This is the main constructor for the Card class.
+        /// It takes two parameters - face (Face) - suit (Suit)
+        /// </summary>
+        /// <param name="face"></param>
+        /// <param name="suit"></param>
+        public Card(Face face, Suit suit)
+        {
+            this.Face = face;
+            this.Suit = suit;
+        }
+
+
+        // PRIVATE METHODS
+
+        // PUBLIC METHODS
+    }
+}
