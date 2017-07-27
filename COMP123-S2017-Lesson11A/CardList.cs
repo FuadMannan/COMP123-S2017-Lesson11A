211193b5c7623686ce13540fd279833a027c36e8
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-07-27 11:14:14 -0400

    Added CardList superclass and refactored Deck class

---
+++ COMP123-S2017-Lesson11A/CardList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Tom Tsiliopoulos
+ * Date: July 25, 2017
+ * Description: This is the abstractl superclass CardList.
+ * It inherits from the build-in List generic collection and consumes the Card class
+ * Version: 0.1 - Created the CardList class
+ */
+
+namespace COMP123_S2017_Lesson11A
+{
+    public abstract class CardList: List<Card>
+    {
+        // PRIVATE INSTANCE VARIABLES
+
+        // PUBLIC PROPERTIES
+
+        // CONSTRUCTORS
+        public CardList()
+        {
+            this._initialize();
+        }
+
+        // PRIVATE METHODS
+        protected abstract void _initialize();
+
+        // PUBLIC METHODS
+    }
+}
