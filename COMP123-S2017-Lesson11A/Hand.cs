9b70f0200e23d9ccb2431da7b06f49067e893652
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-07-27 11:34:14 -0400

    Added Deal1 method to Hand class

--- COMP123-S2017-Lesson11A/Hand.cs
+++ COMP123-S2017-Lesson11A/Hand.cs
@@ -8,7 +8,7 @@ using System.Text;
  * Date: July 27, 2017
  * Description: This is the Hand class
  * It inherits from the CardList class
- * Version: 0.1 - Created the Hand class
+ * Version: 0.2 - Added the Deal1 method
  */
 
 namespace COMP123_S2017_Lesson11A
@@ -45,6 +45,19 @@ namespace COMP123_S2017_Lesson11A
             }
 
             return outputString;
+        }
+
+
+        /// <summary>
+        /// This method removes a Card from element 0 in the List and returns this Card.
+        /// </summary>
+        public Card Deal1()
+        {
+            Card firstCard = (Card)this[0].Clone();
+            this.RemoveAt(0); // removes the top card
+
+            Console.WriteLine("Deck Contains: " + this.Count + " Cards");
+            return firstCard;
         }
 
 
